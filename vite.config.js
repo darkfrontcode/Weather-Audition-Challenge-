c1236d32ba2abb26bc4f72863f930fb260957948
darkfrontcode <darkfrontcode@gmail.com>
2023-04-15 23:07:29 -0300

    1 file modified

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,14 @@ export default defineConfig({
       configureServer: (server) => {
         server.middlewares.use((_req, res, next) => {
           res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+          );
+          res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, Content-Type, X-Auth-Token'
+          );
           next();
         });
       },
