a2f6348787457204ec013013818adeaf40090df5
darkfrontcode <darkfrontcode@gmail.com>
2023-04-15 23:03:59 -0300

    1 file modified

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
+/*
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     cors: false,
   },
+});
+*/
+
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    {
+      name: 'configure-response-headers',
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          next();
+        });
+      },
+    },
+  ],
 });
