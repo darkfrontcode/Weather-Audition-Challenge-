dc218d0e3a7d4274c70bd0d98b93a252ed6ebb31
darkfrontcode <darkfrontcode@gmail.com>
2023-04-17 00:17:18 -0300

    1 file modified

--- src/domain/weather/hooks/__test__/weather.hook.test.ts
+++ src/domain/weather/hooks/__test__/weather.hook.test.ts
@@ -1,11 +1,18 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { IPeriod } from '../..';
 import { useWeather } from '../weather.hook';
 
 describe('[hook] Weather', () => {
   describe('SHOULD save forecast weather', () => {
     test('WHEN a change happens', () => {
-      const { result, rerender } = renderHook(() => useWeather());
+      const { result } = renderHook(() => useWeather());
       expect(result.current.forecast).toEqual([]);
+
+      act(() => {
+        result.current.change([{} as IPeriod]);
+      });
+
+      expect(result.current.forecast).not.toEqual([]);
     });
   });
 });
